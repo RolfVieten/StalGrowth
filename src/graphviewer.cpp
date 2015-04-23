033b87b7d0a658a407325a31718ed1c194f18438
Francisco Hernandez <Fran89@users.noreply.github.com>
2015-04-23 15:56:39 -0400

    Added second graph

--- src/graphviewer.cpp
+++ src/graphviewer.cpp
@@ -26,14 +26,24 @@ void GraphViewer::setresult(DataItem data, Results result){
 
 void GraphViewer::setGraph(){
     ui->Graph->plotLayout()->clear();
+    // Input
     QCPAxisRect *wideAxisRect = new QCPAxisRect(ui->Graph);
     wideAxisRect->setupFullAxesBox(true);
     wideAxisRect->addAxis(QCPAxis::atLeft)->setTickLabelColor(Qt::red); // add an extra axis on the left and color its numbers
     wideAxisRect->addAxis(QCPAxis::atLeft)->setTickLabelColor(Qt::green); // add an extra axis on the left and color its numbers
     wideAxisRect->axis(QCPAxis::atBottom)->setTickLabelType(QCPAxis::ltDateTime);
     wideAxisRect->axis(QCPAxis::atBottom)->setDateTimeFormat("MMMM\nyyyy");
+    // Output
+    QCPAxisRect *wideAxisRect1 = new QCPAxisRect(ui->Graph);
+    wideAxisRect1->addAxis(QCPAxis::atLeft)->setTickLabelColor(Qt::red); // add an extra axis on the left and color its numbers
+    wideAxisRect1->axis(QCPAxis::atBottom)->setTickLabelType(QCPAxis::ltDateTime);
+    wideAxisRect1->axis(QCPAxis::atBottom)->setDateTimeFormat("MMMM\nyyyy");
+
+    // Add to the Plot
     ui->Graph->plotLayout()->addElement(0,0,wideAxisRect);
-    // Main Graph
+    ui->Graph->plotLayout()->addElement(1,0,wideAxisRect1);
+
+    // Main Graph1
     //Temp
     QCPGraph *mainGraph1 = ui->Graph->addGraph(wideAxisRect->axis(QCPAxis::atBottom), wideAxisRect->axis(QCPAxis::atLeft));
     mainGraph1->setPen(QPen(Qt::black));
@@ -52,6 +62,26 @@ void GraphViewer::setGraph(){
     mainGraph3->setLineStyle(QCPGraph::lsLine);
     mainGraph3->setData(Time.toVector(),Data.cCa.toVector());
     mainGraph3->rescaleAxes();
+
+    // Main Graph 2
+    //Temp
+    QCPGraph *mainGraph12 = ui->Graph->addGraph(wideAxisRect1->axis(QCPAxis::atBottom), wideAxisRect1->axis(QCPAxis::atLeft));
+    mainGraph12->setPen(QPen(Qt::black));
+    mainGraph12->setLineStyle(QCPGraph::lsLine);
+    mainGraph12->setData(Time.toVector(), Result.GrowthRate.toVector());
+    mainGraph12->rescaleAxes();
+    // pCO2
+    QCPGraph *mainGraph22 = ui->Graph->addGraph(wideAxisRect1->axis(QCPAxis::atBottom), wideAxisRect1->axis(QCPAxis::atLeft, 1));
+    mainGraph22->setPen(QPen(Qt::red));
+    mainGraph22->setLineStyle(QCPGraph::lsLine);
+    mainGraph22->setData(Time.toVector(),Result.AppcCa.toVector());
+    // cCa
+    QCPGraph *mainGraph32 = ui->Graph->addGraph(wideAxisRect1->axis(QCPAxis::atBottom), wideAxisRect1->axis(QCPAxis::atLeft, 1));
+    mainGraph32->setPen(QPen(Qt::green));
+    mainGraph32->setLineStyle(QCPGraph::lsLine);
+    mainGraph32->setData(Time.toVector(),Data.cCa.toVector());
+    mainGraph32->rescaleAxes();
+
 
     ui->Graph->replot();
 }
